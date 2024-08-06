24b9f352fed36c4a18d9cf515c0bbe8cf58b96b4
eduschoof <eduardoschoof@gmail.com>
2024-08-05 22:27:20 -0300

    Aula 2

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,19 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+
+from fast_zero.schemas import Message
+
+from fastapi.responses import HTMLResponse
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
-    return 'Olá Mundo!'
+    return {'message': 'Olá Mundo!'}
+
+
+@app.get('/html', status_code=HTTPStatus.OK, response_model=Message)
+def read_html():
+    return HTMLResponse(content='<h1>Olá Mundo!</h1>', status_code=200)
--- tests/test_app.py
+++ tests/test_app.py
@@ -6,9 +6,18 @@ from fast_zero.app import app
 
 
 def test_root_deve_retornar_ok_e_ola_mundo():
+    client = TestClient(app)  # Arrange (organização)
+
+    response = client.get('/')  # Act (ação)
+
+    assert response.status_code == HTTPStatus.OK  # Assert (verificação)
+    assert response.json() == {'message': 'Olá Mundo!'}
+
+
+def test_html_deve_retornar_ok_e_ola_mundo_em_html():
     client = TestClient(app)
 
-    response = client.get('/')
+    response = client.get('/html')
 
     assert response.status_code == HTTPStatus.OK
-    assert response.json() == 'Olá Mundo!'
+    assert response.text == '<h1>Olá Mundo!</h1>'
