703bdb673c149f7c55a8d0719b6ef25499fae241
eduschoof <eduardoschoof@gmail.com>
2024-08-20 18:15:38 -0300

    add arquivo database e integrando banco de bados (sqlalchemy)ao FastAPI

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,7 @@
 from http import HTTPStatus
 
+from fast_zero.schemas import UserPublic
+
 
 def test_root_deve_retornar_ok_e_ola_mundo(client):
     response = client.get('/')  # Act (ação)
@@ -9,85 +11,53 @@ def test_root_deve_retornar_ok_e_ola_mundo(client):
 
 
 def test_create_user(client):
-    response = client.post(  # UserSchema
+    response = client.post(
         '/users/',
         json={
-            'username': 'testeusername',
-            'email': 'teste@teste.com',
-            'password': '123456',
+            'username': 'alice',
+            'email': 'alice@example.com',
+            'password': 'secret',
         },
     )
-    #  voltou 201 o status code correto?
     assert response.status_code == HTTPStatus.CREATED  # 201
-    #  Validar UserPublic
     assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
         'id': 1,
-        'username': 'testeusername',
-        'email': 'teste@teste.com',
     }
 
 
 def test_read_users(client):
     response = client.get('/users/')  # Act
-
     assert response.status_code == HTTPStatus.OK  # Assert
-    assert response.json() == {
-        'users': [
-            {'username': 'testeusername', 'email': 'teste@teste.com', 'id': 1}
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_read_user(client):
-    response = client.get('/users/1')
-
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {
-        'id': 1,
-        'username': 'testeusername',
-        'email': 'teste@teste.com',
-    }
+def test_read_users_with_user(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()  # Arrange
+    response = client.get('/users/')  # Act
+    assert response.json() == {'users': [user_schema]}
 
 
-def test_update_user(client):
+def test_update_user(client, user):
     response = client.put(
         '/users/1',
         json={
-            'username': 'testeusername2',
-            'email': 'teste2@teste.com',
-            'id': 1,
-            'password': '123456',
+            'username': 'bob',
+            'email': 'bob@example.com',
+            'password': 'mynewpassword',
         },
     )
     assert response.status_code == HTTPStatus.OK
     assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
         'id': 1,
-        'username': 'testeusername2',
-        'email': 'teste2@teste.com',
     }
 
 
-def test_update_user_not_found(client):
-    response = client.put(
-        '/users/2',
-        json={
-            'username': 'Eu',
-            'email': 'eu2@teste.com',
-            'password': '212121',
-        },
-    )
-    assert response.status_code == HTTPStatus.NOT_FOUND
-    assert response.json() == {'detail': 'User not found'}
-
-
-def test_delete_user(client):
+def test_delete_user(client, user):
     response = client.delete('/users/1')
 
+    assert response.status_code == HTTPStatus.OK
     assert response.json() == {'message': 'User deleted'}
-
-
-def test_delete_user_not_found(client):
-    response = client.delete('/users/2')
-
-    assert response.status_code == HTTPStatus.NOT_FOUND
-    assert response.json() == {'detail': 'User not found'}
